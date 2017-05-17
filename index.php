6f3025f9e5577d26bc286116465a5e314ee392c2
Jeff Church <jeffmichaelchurch@gmail.com>
2017-05-16 22:18:14 -0500

    Pets

--- index.php
+++ index.php
@@ -28,6 +28,10 @@
         
         $f3->set('preferredCustomer', true);
         $f3->set('lastLogin', strtotime('-1 week'));
+        
+        
+        $pet = mew Pet('Fido', 'pink');
+        $f3->set('myPet', $pet);
         echo Template::instance()->render('pages/info.html');
     });
 
